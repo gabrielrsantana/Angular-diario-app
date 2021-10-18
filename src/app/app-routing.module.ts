f1b0afd79ce51d6efeb7adcc0091d843a1570620
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2021-10-18 16:47:51 -0300

    navbar template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,34 +5,40 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+import {
+  hasCustomClaim,
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+} from '@angular/fire/compat/auth-guard';
+import { canActivate } from '@angular/fire/compat/auth-guard';
+
+const redirectLoggedInToDiarios = () => redirectLoggedInTo(['diarios']);
+//redireciona usuarios nao autorizados para tela login
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Route[] = [
   { path: '', component: HomeComponent },
   {
     path: 'login',
-    component: LoginComponent,
-    
+    component: LoginComponent, //nao acessa se logado,redireciona
+    ...canActivate(redirectLoggedInToDiarios),
   },
   {
     path: 'signup',
-    component: SignupComponent,
-    
+    component: SignupComponent, //nao acessa se estiver logado
+    ...canActivate(redirectLoggedInToDiarios),
   },
   {
     path: 'diarios',
     component: DiariosComponent,
-    
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports:[
-    RouterModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private authService: AuthService) {}
 
-  constructor() { }
+  user$?: Observable<any>;
+  user: any;
 
-  ngOnInit(): void {
+  routerEventSubscription!: Subscription;
+  currentRoute: string = '/';
+  isMenuCollapsed: boolean = true;
+
+  toggle() {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
   }
 
+  close() {
+    this.isMenuCollapsed = true;
+  }
+
+  logout() {
+    this.close();
+    this.authService.logout();
+  }
+
+  ngOnInit(): void {
+    this.routerEventSubscription = this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationStart) {
+        this.currentRoute = ev.url;
+      }
+    });
+
+    this.user$ = this.authService.user;
+  }
+
+  ngOnDestroy(): void {
+    this.routerEventSubscription.unsubscribe();
+  }
 }
