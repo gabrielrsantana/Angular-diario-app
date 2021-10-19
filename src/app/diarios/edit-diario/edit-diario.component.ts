a5b0b083692cffc7652d61f3dd92e8b3a09d820a
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2021-10-19 09:34:10 -0300

    update edit

--- src/app/diarios/edit-diario/edit-diario.component.ts
+++ src/app/diarios/edit-diario/edit-diario.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { DiariosService } from 'src/app/services/diarios.service';
+import { UploadImageService } from 'src/app/services/upload-image.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Diary } from 'src/app/interfaces/diary';
 
 @Component({
   selector: 'app-edit-diario',
   templateUrl: './edit-diario.component.html',
-  styleUrls: ['./edit-diario.component.css']
+  styleUrls: ['./edit-diario.component.css'],
 })
 export class EditDiarioComponent implements OnInit {
+  @Input() diary: Diary = {} as Diary;
 
-  constructor() { }
+  image: File | null = null;
 
-  ngOnInit(): void {
+  changeImage(images: FileList | null) {
+    if (images) {
+      this.image = images[0];
+    }
   }
 
+  onSubmit() {
+    if (this.image) {
+      this.uploadService.uploadImage(this.image, (url) => {
+        this.diary.image = url;
+        this.updateDiary();
+      });
+    } else {
+      this.updateDiary();
+    }
+  }
+
+  //updateDiary ou Diario?
+  updateDiary() {
+    //depois fecha o modal quando atualizar
+    this.diariosService.updateDiary(this.diary).then(this.activeModal.dismiss);
+  }
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private uploadService: UploadImageService,
+    private diariosService: DiariosService
+  ) {}
+
+  ngOnInit(): void {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
+import { Diary } from './../interfaces/diary';
+import { DiariosService } from 'src/app/services/diarios.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  allDiaries$?: Observable<Diary[]>;
+  allDiarios: Diary[] = [];
+  constructor(private diariosService: DiariosService) {}
 
   ngOnInit(): void {
+    this.allDiaries$ = this.diariosService.getAllDiaries();
   }
-
 }
